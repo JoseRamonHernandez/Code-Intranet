5ec0f60eb9be40c686164e24ba360ba3f0487577
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2023-02-09 13:56:27 -0600

    eliminar avisos

---
+++ public/pages/admin/deleteAlert.php
@@ -0,0 +1,72 @@
+<?php
+function exception_error_handler($errno, $errstr, $errfile, $errline)
+{
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+}
+
+set_error_handler("exception_error_handler");
+
+if(!empty($_GET['id']))
+{
+    $id = $_GET['id'];
+
+    try{
+        $datos = json_decode(file_get_contents("https://REST-API.joseramonhernan.repl.co/findAlert/$id"), true);
+          $title = $datos['title'];
+        //  echo $name;
+          $text = $datos['text'];
+        ?>
+    
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <script src="sweetalert2.min.js"></script>
+<link rel="stylesheet" href="sweetalert2.min.css">
+
+</head>
+<body>
+    
+
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>Swal.fire({
+        icon: 'warning',
+        title: 'Seguro que quieres eliminar el aviso: <?php echo $title;?>',
+        text: '<?php echo$text;?>',
+        showDenyButton: true,
+        showCancelButton: false,
+        confirmButtonText: 'Si, Eliminar',
+        denyButtonText: 'Cancelar'
+        
+      }).then((result) => {
+  /* Read more about isConfirmed, isDenied below */
+  if (result.isConfirmed) {
+   
+    window.location="./deleteAlertAnswer.php?id=<?php echo$id;?> " 
+  }else{
+    window.location="./alert.php"
+  } 
+      })     
+      </script>
+</body>
+</html>
+<?php
+}catch(Exception $e){
+    ?>
+    <script>
+    window.location="../err.html"
+  </script>
+  <?php
+}
+}else{
+
+    ?>
+    <script>window.location="./err.html"</script>
+    <?php
+}
+?>

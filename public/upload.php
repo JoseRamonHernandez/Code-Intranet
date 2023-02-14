e1775c1a1c87db32a8ba8f818a272c8e95a5c88d
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2023-02-13 19:48:14 -0600

    upload image and return url

--- public/upload.php
+++ public/upload.php
@@ -19,8 +19,8 @@ use Cloudinary\Api\Upload\UploadApi;
 $upload = new UploadApi();
 echo '<pre>';
 echo json_encode(
-    $upload->upload('https://res.cloudinary.com/demo/image/upload/flower.jpg', [
-        'public_id' => 'flower_sample',
+    $upload->upload('./img/clerprem_mexico_2.jpg', [
+        'public_id' => 'example',
         'use_filename' => TRUE,
         'overwrite' => TRUE]),
     JSON_PRETTY_PRINT
@@ -33,8 +33,27 @@ use Cloudinary\Api\Admin\AdminApi;
 // Get the asset details
 $admin = new AdminApi();
 echo '<pre>';
-echo json_encode($admin->asset('flower_sample', [
-    'asset_id']), JSON_PRETTY_PRINT
-);
-echo '</pre>';
+$info = json_encode($admin->asset('example'), JSON_PRETTY_PRINT); //Almacena la información de la imagen en la variable
+
+
+echo $info; //Imprime la información
+echo ("<hr><br>");
+$obj =(json_decode($info));// Convierte la cadena $info a un objeto JSON
+echo ("<hr><br>");
+echo ("<hr><br>");
+echo$obj->{'url'};// Imprime el campo solicitado
+echo ("<hr><br>");
+
+var_dump($info[7]);
+echo ("<hr><br>");
+//echo $a;
+//echo $info['url'];
+//$datos = json_decode(file_get_contents($info));
+echo ("<hr><br>");
+$data = file_get_contents($info);
+$decoded_json = json_decode($data, true);
+if(isset($decoded_json['url'])){
+//echo $decoded_json['url'];
+}else{echo ("NEl perro no jala");}
+
 ?>

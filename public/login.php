e6d6e5d055279d2c008d66553431e47de83b6acf
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2023-02-28 22:10:15 -0600

    add action Access Denied

--- public/login.php
+++ public/login.php
@@ -71,9 +71,41 @@ if(isset($_GET['ingresar'])==1)
     $id = $datos["_id"];
     $user_type = $datos["user_type"];
     $name = $datos["name"];
+    
 
     if($user_type == "collaborator")
     {
+
+      if($datos['status'] == "baja")
+      {
+        ?>
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <script src="sweetalert2.min.js"></script>
+<link rel="stylesheet" href="sweetalert2.min.css">
+
+</head>
+<body>
+    
+
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>Swal.fire({
+        icon: 'info',
+        title: 'Acceso Denegado',
+        iconColor: '#E41111',
+        
+      })     
+      </script>
+</body>
+</html>
+<?php
+      }else{
     ?>
      <!DOCTYPE html>
 <html lang="en">
@@ -121,6 +153,7 @@ Swal.fire({
       
        <?php
     }
+  }
     elseif ($user_type == "administrator") {
         ?>
         <!DOCTYPE html>

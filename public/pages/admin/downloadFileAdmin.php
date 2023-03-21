ee5f5767589b3bc511f9a9d700da057e04337c7f
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2023-03-20 18:42:18 -0600

    PDF VIEW

--- public/pages/admin/downloadFileAdmin.php
+++ public/pages/admin/downloadFileAdmin.php
@@ -11,27 +11,44 @@ if(!empty(isset($_GET['nameFile'])) && !empty(isset($_GET['nameCourse'])) && !em
 
     $nameFile = $_GET['nameFile'];
 
-        $url = "../documents/$nameFile";
-
-
-
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename='.basename($url));
-        header('Content-Transfer-Encoding: binary');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($url));
-        ob_clean();
-        flush();
-        readfile($url);
-
         ?>
 
-        <script>
-            window.location= "./selectCourses.php?nameCourse=<?php echo $nameCourse; ?>&idCourse=<?php echo $idCourse; ?>&idCategorie=<?php echo $idCategorie; ?>&nameCategorie=<?php echo $nameCategorie; ?>"
-        </script>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>PDF</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+        </head>
+        <body>
+
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+   <div class="container" style="">
+   <p></p>
+   </div>
+      <form class="d-flex" role="search" method="GET" action="selectCourses.php">
+        <input type="hidden" name="idCourse" value="<?php echo $idCourse; ?>">
+        <input type="hidden" name="idCategorie" value="<?php echo $idCategorie; ?>">
+        <input type="hidden" name="nameCategorie" value="<?php echo $nameCategorie; ?>">
+        <input type="hidden" name="nameCourse" value="<?php echo $nameCourse; ?>">
+        <button class="btn btn-outline-success" name="buscar" type="submit">Regresar</button>
+      </form>
+    
+  </div>
+</nav>
+
+        <div class="container">
+        <embed src="../documents/<?php echo $nameFile; ?>" type="application/pdf" width="100%" height="600px" />
+
+        </div>
+            
+        </body>
+        </html>
 
         <?php
 

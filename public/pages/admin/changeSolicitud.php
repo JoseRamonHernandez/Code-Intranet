9d0966067ed36cc3be88609ec817ee4a89f0d197
JoseRamonHernandez <joseramonhernandezmunoz20@gmail.com>
2023-05-17 18:20:58 -0600

    Retoques sobre cursos

--- public/pages/admin/changeSolicitud.php
+++ public/pages/admin/changeSolicitud.php
@@ -45,61 +45,8 @@ else
     }).then((respuesta) => {
         if(respuesta.isConfirmed)
         {
-            <?php
-
-//url de la petición
-  $url = "http://localhost:3000/$id_collaborator/categoriesCompleted";
-  
-  //inicializamos el objeto CUrl
-  $ch = curl_init($url);
-   
-  //el json simulamos una petición de un login
-  $jsonData = array(
-     'id_categorie' => $id_categorie
-  );
-   
-  //creamos el json a partir de nuestro arreglo
-  $jsonDataEncoded = json_encode($jsonData);
-   
-  //Indicamos que nuestra petición sera Post
-  curl_setopt($ch, CURLOPT_POST, 1);
-   
-   //para que la peticion no imprima el resultado como un echo comun, y podamos manipularlo
-   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-   
-  //Adjuntamos el json a nuestra petición
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
-   
-  //Agregamos los encabezados del contenido
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-   
-   //ignorar el certificado, servidor de desarrollo
-            //utilicen estas dos lineas si su petición es tipo https y estan en servidor de desarrollo
-   //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-           //curl_setopt($process, CURLOPT_SSL_VERIFYHOST, FALSE);
-   
-  //Ejecutamos la petición
-  $result = curl_exec($ch);
-  if($result)
-  {
-    ?>
-            Swal.fire({
-                icon: 'success',
-                title: 'Permiso otorgado.',
-                showCancelButton: false,
-                showConfirmButton: true,
-                confirmButtonText: 'Enterado',
-                confirmButtonColor: "blue",
-            }).then ((res) => {
-                if(res)
-                {
-                    window.location="updateSolicitud.php?idSolicitud=<?php echo $idSolicitud; ?>&status=APROBADO";
-                }
-            })
-    <?php
-  }
-  ?>
-
+    
+            window.location="aprovate.php?id_categorie=<?php echo $id_categorie; ?>&idSolicitud=<?php echo $idSolicitud; ?>&id_collaborator=<?php echo $id_collaborator; ?>"
             
         }else if(respuesta.isDenied)
         {
